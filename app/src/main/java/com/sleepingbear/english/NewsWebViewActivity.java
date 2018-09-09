a70c3deecd5c715278ad02bbfbbc827dbfd9ac94
limsm9449 <limsm9449@naver.com>
2018-09-09 23:27:09 +0900

    2018.9.9

--- app/src/main/java/com/sleepingbear/english/NewsWebViewActivity.java
+++ app/src/main/java/com/sleepingbear/english/NewsWebViewActivity.java
@@ -437,7 +437,7 @@ public class NewsWebViewActivity extends AppCompatActivity implements View.OnCli
             script.append("    var e = 0;");
             script.append("    for ( var i = selection.focusOffset - 1; i >= 0; i-- ) {");
             script.append("        if ( fullStr.substring(i, i+1) == \" \" ) {");
-            script.append("            s = i;");
+            script.append("            s = i + 1;");
             script.append("            break;");
             script.append("        }");
             script.append("    }");
@@ -660,9 +660,9 @@ public class NewsWebViewActivity extends AppCompatActivity implements View.OnCli
             } else {
                 DicUtils.dicLog("url = " + url);
                 view.loadUrl(url);
-
-                return true;
             }
+
+            return true;
         }
 
         @Override
